901d0209ddd94b752e00740ae2060e5504ea7b00
skyylex <yury.lapitsky@gmail.com>
2016-05-05 20:10:45 +0300

    Insertion sort was added to samples

---
+++ samples/insertion_sort.py
@@ -0,0 +1,21 @@
+with open("data.input") as source_file:
+    import pickle
+    a_list = pickle.load(source_file)
+
+def insertion_sort(seq):
+    for n in range(1, len(seq)):
+        item = seq[n]
+        hole = n
+        while hole > 0 and seq[hole - 1] > item:
+            seq[hole] = seq[hole - 1]
+            hole = hole - 1
+        seq[hole] = item
+
+
+    return seq
+
+result = insertion_sort(a_list)
+
+with open("data.output", "w") as output:
+    import pickle
+    output.write(str(result))
